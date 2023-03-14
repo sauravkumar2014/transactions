b4a4b68e0770cb208e1484b0463e0ab2febec180
Saurav Kumar <skumar8@paloaltonetworks.com>
2023-03-14 13:25:15 +0530

    Update parent controller to return paginated and sorted parent data

--- src/main/java/com/daofab/transactions/controller/ParentController.java
+++ src/main/java/com/daofab/transactions/controller/ParentController.java
@@ -1,10 +1,14 @@
 package com.daofab.transactions.controller;
 
+import com.daofab.transactions.assembler.ParentModelAssembler;
+import com.daofab.transactions.dto.Parent;
 import com.daofab.transactions.model.ParentModel;
 import com.daofab.transactions.service.ParentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +22,15 @@ public class ParentController {
     @Autowired
     private ParentService parentService;
 
+    @Autowired
+    private ParentModelAssembler parentModelAssembler;
+
+    @Autowired
+    private PagedResourcesAssembler<Parent> pagedParentAssembler;
+
     @GetMapping()
     public PagedModel<ParentModel> getPaginated(@PageableDefault(sort = "id", size = PAGE_SIZE) Pageable pageable) {
-        return null;
+        Page<Parent> parentPage = parentService.getPaginatedParents(pageable);
+        return pagedParentAssembler.toModel(parentPage, parentModelAssembler);
     }
 }
