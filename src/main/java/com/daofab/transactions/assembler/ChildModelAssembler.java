ff362ee0cf2d682b0b0d2988aa89df7825c49d44
Saurav Kumar <skumar8@paloaltonetworks.com>
2023-03-14 14:00:03 +0530

    Add child assembler to derive view model from entity

---
+++ src/main/java/com/daofab/transactions/assembler/ChildModelAssembler.java
@@ -0,0 +1,26 @@
+package com.daofab.transactions.assembler;
+
+import com.daofab.transactions.controller.ChildController;
+import com.daofab.transactions.dto.Child;
+import com.daofab.transactions.model.ChildModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChildModelAssembler extends RepresentationModelAssemblerSupport<Child, ChildModel> {
+    public ChildModelAssembler() {
+        super(ChildController.class, ChildModel.class);
+    }
+
+    @Override
+    public ChildModel toModel(Child entity) {
+        ChildModel model = new ChildModel();
+        model.setId(entity.getId());
+        model.setReceiver(entity.getParent().getReceiver());
+        model.setSender(entity.getParent().getSender());
+        model.setTotalAmount(entity.getParent().getTotalAmount());
+        model.setPaidAmount(entity.getPaidAmount());
+
+        return model;
+    }
+}
