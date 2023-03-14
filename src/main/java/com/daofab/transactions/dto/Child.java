0e82d044876ea92e8d28a9afb3316f603cdfca84
Saurav Kumar <skumar8@paloaltonetworks.com>
2023-03-14 11:25:03 +0530

    Add dto for child and parent transactions

---
+++ src/main/java/com/daofab/transactions/dto/Child.java
@@ -0,0 +1,30 @@
+package com.daofab.transactions.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Child {
+
+    @Id
+    @NonNull
+    private Long id;
+
+    @NonNull
+    private Long parentId;
+
+    @NonNull
+    private Long paidAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "parentId", insertable=false, updatable=false)
+    private Parent parent;
+}
---
+++ src/main/java/com/daofab/transactions/dto/Parent.java
@@ -0,0 +1,33 @@
+package com.daofab.transactions.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Parent {
+
+    @Id
+    @NonNull
+    private Long id;
+
+    @NonNull
+    private String sender;
+
+    @NonNull
+    private String receiver;
+
+    @NonNull
+    private Long totalAmount;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Child> children;
+}
