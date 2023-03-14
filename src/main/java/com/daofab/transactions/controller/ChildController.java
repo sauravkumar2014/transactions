dacbde262ad349ee02735708bb5adeb4b06251fa
Saurav Kumar <skumar8@paloaltonetworks.com>
2023-03-14 14:23:50 +0530

    Update child controller to return child data of specific parent

--- src/main/java/com/daofab/transactions/controller/ChildController.java
+++ src/main/java/com/daofab/transactions/controller/ChildController.java
@@ -1,5 +1,6 @@
 package com.daofab.transactions.controller;
 
+import com.daofab.transactions.assembler.ChildModelAssembler;
 import com.daofab.transactions.model.ChildModel;
 import com.daofab.transactions.service.ChildService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping(path = "/children")
 public class ChildController {
@@ -16,8 +23,17 @@ public class ChildController {
     @Autowired
     private ChildService childService;
 
+    @Autowired
+    private ChildModelAssembler childModelAssembler;
+
     @GetMapping()
     public CollectionModel<ChildModel> getChildren(@RequestParam("parentId") Long parentId) {
-        return null;
+        List<ChildModel> children = childService.getChildren(parentId).stream()
+                .map(childModelAssembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(children,
+                linkTo(methodOn(ChildController.class).getChildren(parentId))
+                        .withSelfRel());
     }
 }
